58a7ca1f109712eda110bff60be931c51864d6ab
Brandt <emailme@brandtryan.com>
2023-06-24 08:55:13 -0400

    Initial commit

---
+++ classes/formalCause.js
@@ -0,0 +1,26 @@
+let img;
+
+function preload() {
+	img = loadImage('assets/statueForm.svg');
+}
+
+class EfficientCause {
+	constructor() {
+		this.x = 100;
+		this.y = 400;
+		this.w = 541;
+		this.h = 1050;
+	}
+
+	// move() {
+	// 	this.x = this.x + random(-5, 5);
+	// 	this.y = this.y + random(-5, 5);
+	// }
+
+	display() {
+		textSize(32);
+		fill(205);
+		text('Efficient', this.x, this.y);
+		image(img, 250, 360, this.w, this.h);
+	}
+}
